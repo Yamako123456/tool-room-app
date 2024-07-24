bd240220833ba3f3e9423bae6e7194df295b2e28
yamako <yamakodev@gmail.com>
2024-07-24 16:13:15 -0700

    fixed bug in nave

--- src/App.tsx
+++ src/App.tsx
@@ -15,32 +15,31 @@ export const App = () => {
 
     <div>
       <Router>
+
         <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
           <div className='container-fluid'>
             <Link className='navbar-brand' to='/'>Toolroom Management System</Link>
-          </div>
-          {/* <button className='navbar-toggler' type='button'
-                data-bs-toggle='collapse'
-                data-bs-target='#contents'>
-            <span className='navbar-toggler-icon'></span>  
-          </button> */}
-          <button className="navbar-toggler" type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#contents">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-          <div className='collapse navbar-collapse' id='contents'>
-            <ul className='navbar-nav'>
-              <li className='nav-item'>
-                <Link to="/" className='nav-link'>Home</Link>
-              </li>
-              <li className='nav-item'>
-                <Link to="/items" className='nav-link'>Items</Link>
-              </li>
-              <li className='nav-item'>
-                <Link to="/about" className='nav-link'>About</Link>
-              </li>
-            </ul>
+          
+         
+            <button className="navbar-toggler" type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#contents">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className='collapse navbar-collapse' id='contents'>
+              <ul className='navbar-nav'>
+                <li className='nav-item'>
+                  <Link to="/" className='nav-link '>Home</Link>
+                </li>
+                <li className='nav-item'>
+                  <Link to="/items" className='nav-link'>Items</Link>
+                </li>
+                <li className='nav-item'>
+                  <Link to="/about" className='nav-link'>About</Link>
+                </li>
+              </ul>
+            </div>           
+
           </div>
         </nav>
       
