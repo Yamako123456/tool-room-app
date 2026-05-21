c68d2b8ba57db5c2ccd61f6afd27ea4b8d2086b0
Miyoko Yamakawa <yamakodev@gmail.com>
2026-05-20 18:14:13 -0700

    Hero screen button caption change

--- src/components/Home/Hero/Hero.tsx
+++ src/components/Home/Hero/Hero.tsx
@@ -38,7 +38,7 @@ export const Hero = ({ items, setItems, cribs }: Props) => {
               href="/login"
               className="py-3 px-10 text-2xl font-bold text-white bg-lightGreen rounded lg:py-4 hover:opacity-70"
             >
-              Get Started
+              Get Started with Operations
             </a>
           </div>
         </div>
