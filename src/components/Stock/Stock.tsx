b007ba2a8eaed45c265b91341a3031403e712dd1
Miyoko Yamakawa <yamakodev@gmail.com>
2026-05-24 18:48:34 -0700

    Separate StockItemScan module and made Stock.tsx the parent

--- src/components/Stock/Stock.tsx
+++ src/components/Stock/Stock.tsx
@@ -1,64 +1,41 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import { ItemModel } from '../../models/ItemModel';
+import { BinModel } from '../../models/BinsModel';
+import StockItemScan from './StockItemScan/StockItemScan';
 
 interface Props {
   handleLogOut: () => void;
 }
 
+type StockStep =  
+| "scanItem"
+| "selectBin"
+| "notFound"
+| "enterQty"
+| "confirm"
+| "success";
+
 const Stock = ({handleLogOut,}: Props) => {
 
   const navigate = useNavigate();
 
+  const [step, setStep] = useState<StockStep>("scanItem");
   const [scanItemCode, setScanItemCode] = useState<string>("");
+  const [selectedItem, setSelectedItem] = useState<ItemModel | null>(null);
+  const [selectedBin, setSelectedBin] = useState<BinModel | null>(null);
+  const [stockQty, setStockQty] = useState<number>(0);
+
   return (
     <section id='stock'>
-      <div className='relative flex items-center justify-between mb-10'>
-        <div className='flex gap-3'>
-          <button 
-            onClick={handleLogOut}
-            className='mt-1, mb-10 lg:mt-0 lg:ml-auto bg-yellow-500 hover:bg-yellow-600 text-white px-4 py-2 rounded'
-          >
-            Log Out  
-          </button>  
-          <button onClick={() => navigate('/main-menu')}
-            className='mt-1, mb-10 lg:mt-0 lg:ml-auto bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded'
-          >
-            Main Menu  
-          </button>
-        </div>  
-        <div className='absolute left-1/2 lg:-translate-x-1/2'>
-          <h2 className=' text-2xl font-semibold'>
-            Issue Items
-          </h2>
-        </div>
-      </div>
-      <div className='max-w-3xl mx-auto px-6  py-8'>
-        <div className='bg-white rounded-xl shadow-lg border p-6'>
-          <h1 className='text-2xl font-bold mb-2'>
-            Stock Item
-          </h1>
-          <p className='text-sm  text-gray-600 mb-6'>
-            Scan or enter the item code of the product you want to stock.
-          </p>
-          <label className='block text-sm font-medium mb-2'>
-            Item Code Scan or Enter
-          </label>
-          <input 
-            type='text'
-            value={scanItemCode}
-            onChange={(e) => setScanItemCode(e.target.value)}
-            placeholder='Scan or enter item code'
-            className='w-full rounded=md border px-4 py-3 text-lg'
-            autoFocus
-          />
-          <button
-            className='mt-6 w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 rounded-lg'
-            
-          >
-            Search Item
-          </button>
-        </div>
-      </div>
+      {step === "scanItem" && (
+        <StockItemScan
+          handleLogOut={handleLogOut}
+          scanItemCode={scanItemCode}
+          setScanItemCode={setScanItemCode}
+        />
+      )
+      }
 
     </section>
   )
---
+++ src/components/Stock/StockItemScan/StockItemScan.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+interface Props {
+   handleLogOut: () => void;
+   scanItemCode: string;
+   setScanItemCode:React.Dispatch<React.SetStateAction<string>>;
+}
+
+const StockItemScan = ({handleLogOut, scanItemCode, setScanItemCode}: Props) => {
+  
+  const navigate = useNavigate();
+  
+  
+
+return (
+    <section id='stockItemScan'>
+      <div className='relative flex items-center justify-between mb-10'>
+        <div className='flex gap-3'>
+          <button 
+            onClick={handleLogOut}
+            className='mt-1, mb-10 lg:mt-0 lg:ml-auto bg-yellow-500 hover:bg-yellow-600 text-white px-4 py-2 rounded'
+          >
+            Log Out  
+          </button>  
+          <button onClick={() => navigate('/main-menu')}
+            className='mt-1, mb-10 lg:mt-0 lg:ml-auto bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded'
+          >
+            Main Menu  
+          </button>
+        </div>  
+        <div className='absolute left-1/2 lg:-translate-x-1/2'>
+          <h2 className=' text-2xl font-semibold'>
+            Issue Items
+          </h2>
+        </div>
+      </div>
+      <div className='max-w-3xl mx-auto px-6  py-8'>
+        <div className='bg-white rounded-xl shadow-lg border p-6'>
+          <h1 className='text-2xl font-bold mb-2'>
+            Stock Item
+          </h1>
+          <p className='text-sm  text-gray-600 mb-6'>
+            Scan or enter the item code of the product you want to stock.
+          </p>
+          <label className='block text-sm font-medium mb-2'>
+            Item Code Scan or Enter
+          </label>
+          <input 
+            type='text'
+            value={scanItemCode}
+            onChange={(e) => setScanItemCode(e.target.value)}
+            placeholder='Scan or enter item code'
+            className='w-full rounded=md border px-4 py-3 text-lg'
+            autoFocus
+          />
+          <button
+            className='mt-6 w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 rounded-lg'
+            
+          >
+            Search Item
+          </button>
+        </div>
+      </div>
+
+    </section>
+  )
+}
+
+export default StockItemScan
